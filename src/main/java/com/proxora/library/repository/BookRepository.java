e7fd91ed2bc0d52943de6f8c3214c476d0ae1ade
Andreas Keefer <andreas.keefer@proxora.com>
2020-11-11 14:46:53 +0100

    typo

--- src/main/java/com/proxora/library/repository/BookRepository.java
+++ src/main/java/com/proxora/library/repository/BookRepository.java
@@ -9,7 +9,7 @@ class BookRepository {
 
     BookRepository() {
         this.data = Arrays.asList(
-                new Book("1", "Cean Code", "Robert C. Martin", LocalDate.of(2008, 8, 1), 431),
+                new Book("1", "Clean Code", "Robert C. Martin", LocalDate.of(2008, 8, 1), 431),
                 new Book("2", "The Clean Coder", "Robert C. Martin", LocalDate.of(2011, 5, 13), 377),
                 new Book("3", "Clean Architecture", "Robert C. Martin", LocalDate.of(2017, 9, 20), 349),
                 new Book("4", "Harry Potter - The Chamber of Secrets", "J. K. Rowling", LocalDate.of(1998, 7, 2), 431),
